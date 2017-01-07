a82a419c439172824c1f71cc832123cd1316d051
Ryan Frazier <pianomanfrazier@gmail.com>
2017-01-07 12:57:54 -0700

    fix socket push problem

--- io/myio.js
+++ io/myio.js
@@ -1,13 +1,26 @@
 module.exports = function(app) {
   app.users = 0;
+  app.cars = [0,0,0,0];
   app.io.on('connection', function(socket){
     app.users++;
     socket.emit('user_count', app.users);
+    app.io.sockets.emit('update_cars',app.cars);
     console.log('a user connected\nnumber of users: ' + app.users);
+
     socket.on('disconnect', function(){
       app.users--;
       socket.emit('user_count', app.users);
       console.log('a user disconnected\nnumber of users: ' + app.users);
     });
+
+    socket.on('move_car', function(msg) {
+      console.log(msg); 
+      var shift = parseInt(msg.shift);
+      var id = parseInt(msg.id);
+      app.cars[id - 1] = shift;
+      console.log(app.cars);
+      app.io.sockets.emit('update_cars',app.cars);
+    });
+
   });
 }
--- public/javascripts/client_io.js
+++ public/javascripts/client_io.js
@@ -2,23 +2,33 @@ var socket = io();
 var shift = 0;
 var user_id = "";
 
+$(document).ready(function() {
+  socket.emit('get_update'); 
+});
+
 socket.on('user_count', function(msg) {
  $('#status').text(msg); 
  user_id = msg;
 });
 
-
+socket.on('update_cars', function(msg) {
+  console.log('update cars');
+  for (i = 1; i <= 4; i++) {
+    $('#player' + i + ' > img').css("left", msg[i - 1] + "px");
+    console.log(msg[i-1].toString());
+  }
+});
 
 $(document).keydown(function(e){
   if (e.keyCode == 37) { 
     console.log("left pressed");
     shift -= 10;
-    $('#player' + user_id + ' > img').css("left", shift.toString() + "px")
+    socket.emit('move_car', { "id" : user_id , "shift" : shift });
     return false;
   } else if (e.keyCode == 39) {
     console.log("right pressed");
     shift += 10;
-    $('#player' + user_id + ' > img').css("left", shift.toString() + "px")
+    socket.emit('move_car', { "id" : user_id , "shift" : shift });
     return false;
   }
 });
